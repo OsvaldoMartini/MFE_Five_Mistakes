75d150e520db5ad0cdceaea1127806c5393f900b
Osvaldo Martini <martinio@dhl.com>
2022-09-19 18:02:40 +0200

    MyUserMFE Function Component and Type Definition

--- mfe-shared/src/index.ts
+++ mfe-shared/src/index.ts
@@ -1,5 +1,6 @@
 import React from "react";
 
 export type Counter = React.FunctionComponent<{name: string}>;
-
+export type UserMFE = React.FunctionComponent<{user: string}>;
+  
 export default true;
---
+++ remote/src/MyUserMFE.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyUserMFE: React.FunctionComponent<{ user: string }> = ({ user }) => {
+  return
+  (
+    <div>
+      User: {user}</div>
+  );
+};
+
+export default MyUserMFE;
